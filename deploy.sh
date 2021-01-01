bf7787c5440ae59177fba88bcbbfce7cf27d9496
Prakhar Agnihotri <rounak316@gmail.com>
2021-01-01 22:14:56 +0530

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+ng build --prod --output-path=/Users/meddler/Documents/GitHub/face
+cd /Users/meddler/Documents/GitHub/face
+git init
+git remote add origin git@github.com:meddler-xyz/face.git 
+git checkout -b main
+echo "meddler.io" > CNAME
+cp index.html 404.html
+git add .
+git commit -m "Autoated Build"
+git push --set-upstream origin main --force
+
+SHUBHAM SINGH #meddler #blueTeam #segmentationTesting
